e46cfca58fb2cf59bfdcb35d0d69617b66734014
okatoue <omarkatoue@gmail.com>
2024-02-28 20:06:15 -0800

    Updated

--- stream/settings.py
+++ stream/settings.py
@@ -11,10 +11,12 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'yasgasgasghasdrewfdSsdfhsdfhsdfdgfdgsafwaefsdfhsjy')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
