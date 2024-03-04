9f2932aafd7a483623dbe75a979b8a7f8be2167b
okatoue <omarkatoue@gmail.com>
2024-03-04 01:39:27 -0800

    Updated stream to GDSC

--- GDSC/wsgi.py
+++ GDSC/wsgi.py
@@ -11,7 +11,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'stream.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'GDSC.settings')
 
 application = get_wsgi_application()
 
