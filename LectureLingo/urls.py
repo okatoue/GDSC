2032556f44e501e94f49bc6a89499cb4a388ba08
okatoue <omarkatoue@gmail.com>
2024-03-05 03:43:55 -0800

    Deploy

--- LectureLingo/urls.py
+++ LectureLingo/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import path
 
 urlpatterns = [
-    path('', include('transcript.urls')),
     path('admin/', admin.site.urls),
+    path('index/', include('transcript.urls')),
+    path('', include('transcript.urls')),
 ]
