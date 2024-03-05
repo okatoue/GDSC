16ccbd6f7b3137dd935380de63dcd6a73d3a924d
okatoue <omarkatoue@gmail.com>
2024-03-04 18:11:11 -0800

    Deploy

--- LectureLingo/settings.py
+++ LectureLingo/settings.py
@@ -117,6 +117,8 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATIC_ROOT = '/home/ubuntu/LectureLingo/staticfiles/'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
