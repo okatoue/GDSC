1dff628f3efe12e96a4f42903b9a888b3402f546
okatoue <omarkatoue@gmail.com>
2024-03-06 05:33:28 -0800

    Deploy

--- LectureLingo/settings.py
+++ LectureLingo/settings.py
@@ -32,6 +32,7 @@ INSTALLED_APPS = [
     'channels',
     'transcript',
     'templates',
+    'LectureLingo',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
