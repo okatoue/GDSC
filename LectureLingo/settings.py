624c89c9760e9aa9756dabcdbcfd86c5f0ded6a1
okatoue <omarkatoue@gmail.com>
2024-03-06 04:52:26 -0800

    Deploy

--- LectureLingo/settings.py
+++ LectureLingo/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'LectureLingo.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, "transcript/templates/")],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
